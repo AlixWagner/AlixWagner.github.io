d2116fba8379c34892621c7476eed57af918ea47
Alix <alixwagner@yahoo.ca>
2022-03-01 14:57:24 -0500

    base header styling started

--- javascript/app.js
+++ javascript/app.js
@@ -1,11 +1,13 @@
-// eventListener for click on cards to flip 
+// eventListener for click on cards to flip
 // SECONDARY EVENTlISTENER FOR KEYPRESS!
-// toggle "flip" class - rotateY 180deg 
+// toggle "flip" class - rotateY 180deg
 // NOTE TO SELF --- CSS backface-visibility: hidden and transform-style: preserve-3d
 
 // event listener on click of down arrow to scroll to about
 
 // event listener on click of back to top arrow
+
+// PLEASE CONSIDER --- stretch goal --- blobs (.floatingBubblegit checkout) jiggle on click
 
 
 
@@ -13,6 +15,10 @@ const cards = document.querySelectorAll(".projectCard");
 
 function flipCard(event) {
     this.classList.toggle("flip");
-}
+};
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
+
+// establish App Object:
+const myPortfolio = {};
+
